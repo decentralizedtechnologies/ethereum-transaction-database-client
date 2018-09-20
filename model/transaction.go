c2009444d513a59d638045f8f2aa6aa24abb8e1f
Gus <yo@soygus.com>
2018-09-20 10:24:25 -0600

    Removes transaction_label model Adds transaction fields: created_at, status, completed_at, timeout, webhook_on_success, webhook_on_timeout

--- model/transaction.go
+++ model/transaction.go
@@ -9,8 +9,15 @@ import (
 
 // Transaction describes the data structure for an Ethereum tx request
 type Transaction struct {
-	Hash string `gorm:"column:hash" json:"hash"`
-	From string `gorm:"column:from" json:"from"`
+	Hash             string `gorm:"column:hash" json:"hash,omitempty"`
+	From             string `gorm:"column:from" json:"from,omitempty"`
+	Status           string `gorm:"column:status" json:"status,omitempty"`
+	Network          string `gorm:"column:network" json:"network,omitempty"`
+	CreatedAt        int64 `gorm:"column:created_at" json:"created_at,omitempty"`
+	CompletedAt      int64 `gorm:"column:completed_at" json:"completed_at,omitempty"`
+	Timeout          int64 `gorm:"column:timeout" json:"timeout,omitempty"`
+	WebhookOnSuccess string `gorm:"column:webhook_on_success" json:"webhook_on_success,omitempty"`
+	WebhookOnTimeout string `gorm:"column:webhook_on_timeout" json:"webhook_on_timeout,omitempty"`
 }
 
 // Table : gets database table instance
