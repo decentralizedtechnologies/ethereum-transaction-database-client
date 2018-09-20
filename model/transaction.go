8f3e835f93425d8fadb95f4bd1f4ecff6c2361cc
Gus <yo@soygus.com>
2018-09-20 17:59:55 -0600

    Adds IsWebhookCalled int8 type to struct

--- model/transaction.go
+++ model/transaction.go
@@ -38,12 +38,13 @@ type Transaction struct {
 	Hash             string         `gorm:"column:hash" json:"hash,omitempty"`
 	From             string         `gorm:"column:from" json:"from,omitempty"`
 	Status           sql.NullString `gorm:"column:status" json:"status,omitempty"`
+	IsWebhookCalled  int8           `gorm:"column:is_webhook_called" json:"isWebhookCalled,omitempty"`
 	Network          string         `gorm:"column:network" json:"network,omitempty"`
-	CreatedAt        int64          `gorm:"column:created_at" json:"created_at,omitempty"`
-	CompletedAt      int64          `gorm:"column:completed_at" json:"completed_at,omitempty"`
+	CreatedAt        int64          `gorm:"column:created_at" json:"createdAt,omitempty"`
+	CompletedAt      int64          `gorm:"column:completed_at" json:"completedAt,omitempty"`
 	Timeout          int64          `gorm:"column:timeout" json:"timeout,omitempty"`
-	WebhookOnSuccess string         `gorm:"column:webhook_on_success" json:"webhook_on_success,omitempty"`
-	WebhookOnTimeout string         `gorm:"column:webhook_on_timeout" json:"webhook_on_timeout,omitempty"`
+	WebhookOnSuccess string         `gorm:"column:webhook_on_success" json:"webhookOnSuccess,omitempty"`
+	WebhookOnTimeout string         `gorm:"column:webhook_on_timeout" json:"webhookOnTimeout,omitempty"`
 }
 
 // Table : gets database table instance
